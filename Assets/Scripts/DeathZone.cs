d770a4a626f4bb72c14fdb83cdeecb82a2ec30b1
Rarrix <rarrix1@gmail.com>
2018-01-19 22:17:14 +0200

    Main Menu Tweaking
    New Intro
    Sounds
    Gameplay fixes (LUL)

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -6,12 +6,24 @@ using UnityEngine.SceneManagement;
 public class DeathZone : MonoBehaviour
 {
 
+    public AudioSource DeathSoundSource;
+    public AudioClip DeathSound;
+    public GameObject Camera;
+    public Animation Death;
+    public Animator Animator;
+
+
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "vihree" || other.gameObject.tag == "oranssi"  )
         {
+           Camera.GetComponent<Animation>().Play();
+         Death.Play();
+            DeathSoundSource.PlayOneShot(DeathSound);
             Debug.Log("hit");
-            Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+            
+            //Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
         }
         else
         {
